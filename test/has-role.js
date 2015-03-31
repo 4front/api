bc65e4b3635092237f72030e2495ee0a4c1fd24a
David Von Lehman <david.vonlehman@gmail.com>
2015-03-30 19:42:39 -0700

    More unit tests

---
+++ test/has-role.js
@@ -0,0 +1,76 @@
+var supertest = require('supertest');
+var _ = require('lodash');
+var express = require('express');
+var shortid = require('shortid');
+var assert = require('assert');
+var sinon = require('sinon');
+var debug = require('debug')('4front-api:test');
+var hasRole = require('../lib/middleware/has-role');
+var helper = require('./helper');
+
+describe('hasRole()', function() {
+  var self;
+
+  beforeEach(function() {
+    self = this;
+    this.server = express();
+
+    this.user = {
+      userId: shortid.generate(),
+      username: 'tester',
+      secretKey: shortid.generate()
+    };
+
+    this.orgMember = {
+      role: 'admin',
+      orgId: shortid.generate(),
+      userId: this.user.userId
+    };
+
+    this.server.use(function(req, res, next) {
+      debug("initialize req.ext");
+      req.ext = {
+        user: self.user,
+        orgMember: self.orgMember
+      };
+
+      next();
+    });
+
+    this.requiredRoles = null;
+
+    this.server.get('/', function(req, res, next) {
+      hasRole(self.requiredRoles)(req, res, function(err) {
+        if (err)
+          return next(err);
+        res.json(req.ext);
+      });
+    });
+
+    this.server.use(helper.errorHandler);
+  });
+
+  it('has valid roles ', function(done) {
+    this.orgMember.role = 'admin';
+    this.requiredRoles = ['admin', 'contributor'];
+
+    supertest(this.server)
+      .get('/')
+      .expect(200)
+      .end(done);
+  });
+
+  it('returns 401 when lacking required role', function(done) {
+    this.orgMember.role = 'readonly';
+    this.requiredRoles = 'admin,contributor';
+
+    supertest(this.server)
+      .get('/')
+      .expect(401)
+      .expect(function(res) {
+        assert.equal(res.body.code, 'lackRequiredRole');
+        assert.ok(_.isEqual(res.body.requiredRole, ['admin', 'contributor']));
+      })
+      .end(done);
+  });
+});
---
+++ test/routes.apps.js
@@ -0,0 +1,80 @@
+var supertest = require('supertest');
+var express = require('express');
+var shortid = require('shortid');
+var assert = require('assert');
+var sinon = require('sinon');
+var debug = require('debug')('4front-api:test');
+var appsRoute = require('../lib/routes/apps');
+var helper = require('./helper');
+
+describe('routes/apps', function() {
+  var self;
+
+  beforeEach(function() {
+    self = this;
+    this.server = express();
+
+    this.userId = shortid.generate();
+
+    this.user = {
+      userId: shortid.generate(),
+      username: 'tester',
+      secretKey: shortid.generate()
+    };
+
+    this.server.use(function(req, res, next) {
+      req.ext = {
+        user: self.user
+      };
+
+      next();
+    });
+
+    this.options = {
+      database: {
+        getUser: function(userId, callback) {
+          callback(null, self.user);
+        },
+        getAppName: function(name, callback) {
+          callback(null, self.appName);
+        }
+      },
+      appLookup: function(query, settings, callback) {
+        callback(null, null);
+      }
+    };
+
+    // Register middleware for handling the appId parameter
+    this.server.use(appsRoute(this.options));
+
+    this.server.use(helper.errorHandler);
+  });
+
+  describe('HEAD /:appName', function() {
+    it('existing app name', function(done) {
+      var appName = "appname";
+      this.options.appLookup = function(query, settings, callback) {
+        callback(null, {
+          appId: shortid.generate(),
+          name: appName
+        });
+      };
+
+      supertest(this.server)
+        .head('/' + appName)
+        .expect(200)
+        .end(done);
+    });
+
+    it('non existing app name', function(done) {
+      this.options.appLookup = function(query, settings, callback) {
+        callback(null, null);
+      };
+
+      supertest(this.server)
+        .head('/someappname')
+        .expect(404)
+        .end(done);
+    });
+  });
+});
